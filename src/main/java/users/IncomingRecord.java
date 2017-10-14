d8a36e0b6cdc5f05069dbca5bdf7b9cf6575e311
Unknown <ajhonts@uwm.edu>
2017-10-14 15:13:17 -0500

    Buttons On The Web Page

---
+++ src/main/java/users/IncomingRecord.java
@@ -0,0 +1,46 @@
+package users;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class IncomingRecord {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Integer id;
+
+    private String name;
+
+    private String food;
+
+    private Double weight;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFood() {
+        return food;
+    }
+
+    public void setFood(String email) {
+        this.food = email;
+    }
+
+    public Double getWeight() { return weight; }
+
+    public void setWeight(Double weight) { this.weight = weight; }
+}
---
+++ src/main/java/users/IncomingRecordRepository.java
@@ -0,0 +1,5 @@
+package users;
+import org.springframework.data.repository.CrudRepository;
+
+public interface IncomingRecordRepository extends CrudRepository<IncomingRecord, Long> {
+}
